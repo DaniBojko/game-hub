fd7f5d1681ef9b70d4a30f21f0b52074d9a8f4b8
Dani Bojko <bojko.dani2@gmail.com>
2023-06-17 16:56:15 +0200

    Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Button colorScheme="teal" variant="outline" size="md">
-      Button
-    </Button>
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="tomato">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="gold">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
