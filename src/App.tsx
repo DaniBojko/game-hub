05615961c0ef80c1e50703414badc6f753540f15
Dani Bojko <bojko.dani2@gmail.com>
2023-06-17 17:16:38 +0200

    NavBar mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="tomato">
