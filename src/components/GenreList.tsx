3ca6fc8dd9ba3e5b9f5fe4a47bfb2bbdcf70f297
Dani Bojko <bojko.dani2@gmail.com>
2023-06-22 19:01:13 +0200

    Loading spinner for Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,30 +1,28 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import cropImageURL from "../services/image-url";
-import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  return (
-    <>
-      {error && <p>{error}</p>}
 
-      <List>
-        {data.map((genre) => (
-          <ListItem key={genre.id} paddingY="5px">
-            <HStack>
-              <Image
-                borderRadius={8}
-                boxSize="32px"
-                src={cropImageURL(genre.image_background)}
-              ></Image>
-              <Text fontSize="lg">{genre.name}</Text>
-            </HStack>
-          </ListItem>
-        ))}
-      </List>
-    </>
+  if (error) return null;
+
+  if (isLoading) return <Spinner />;
+  return (
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              borderRadius={8}
+              boxSize="32px"
+              src={cropImageURL(genre.image_background)}
+            ></Image>
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
--- src/components/GenreListSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-import { SkeletonText } from "@chakra-ui/react";
-
-const GenreListSkeleton = () => {
-  return <SkeletonText></SkeletonText>;
-};
-
-export default GenreListSkeleton;
