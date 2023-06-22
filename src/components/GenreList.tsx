62e3e9f2e54e08e4a8a92c425c2d31d631486133
Dani Bojko <bojko.dani2@gmail.com>
2023-06-22 18:27:07 +0200

    Generalizing fetching hooks

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,8 @@
 import useGenres from "../hooks/useGenres";
-import { Genre } from "../hooks/useGenres";
 import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { genres, error, isLoading } = useGenres();
+  const { data, error, isLoading } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
@@ -13,7 +12,7 @@ const GenreList = () => {
           <GenreListSkeleton key={skeleton}></GenreListSkeleton>
         ))}
       <ul>
-        {genres.map((genre: Genre) => (
+        {data.map((genre) => (
           <li key={genre.id}>{genre.name}</li>
         ))}
       </ul>
