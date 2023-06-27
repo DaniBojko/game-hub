5934e5a5ea1b93a1498f035dcafd8153e655ce26
Dani Bojko <bojko.dani2@gmail.com>
2023-06-27 17:04:47 +0200

    optimizing with static data

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,11 @@
-import { HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import {
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import cropImageURL from "../services/image-url";
 import { Button } from "@chakra-ui/react";
@@ -11,32 +18,38 @@ interface Props {
 const GenreList = ({ selectedGenre, setGenre }: Props) => {
   const { data, error, isLoading } = useGenres();
 
-  if (error) return null;
+  //if (error) return null;
 
-  if (isLoading) return <Spinner />;
+  //if (isLoading) return <Spinner />;
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              marginRight={1}
-              borderRadius={8}
-              boxSize="32px"
-              src={cropImageURL(genre.image_background)}
-            ></Image>
-            <Button
-              fontWeight={genre === selectedGenre ? "bolder" : "normal"}
-              onClick={() => setGenre(genre)}
-              variant="link"
-              fontSize="lg"
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading marginBottom={5} fontSize="2xl">
+        Genres
+      </Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                objectFit="cover"
+                marginRight={1}
+                borderRadius={8}
+                boxSize="32px"
+                src={cropImageURL(genre.image_background)}
+              ></Image>
+              <Button
+                fontWeight={genre === selectedGenre ? "bolder" : "normal"}
+                onClick={() => setGenre(genre)}
+                variant="link"
+                fontSize="lg"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -10,7 +10,7 @@ interface Props {
 const PlatformSelector = ({ setPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
 
-  if (error) return;
+  //if (error) return;
 
   return (
     <Menu>
